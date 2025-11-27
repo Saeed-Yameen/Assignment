72cd52e150334229ab9e2030c1b9ff54567bfe8b
Saeed-Yameen <saeedyameen34@gmail.com>
2025-11-27 09:48:38 -0800

    Add files via upload
    calculation done by JS

---
+++ calculation/script.js
@@ -0,0 +1,32 @@
+var i = 18 / (3 + 3) * 4 - (12 - [5 * (2 + 1)] ) + 7 ** 2
+
+
+var i = 18 / 6 * 4 - ( 12 - [5 * (2 + 1)] ) + 7 ** 2
+console.log(3+3);
+ 
+var i = 3 * 4 - ( 12 - [5 * (2 + 1)] ) + 7 ** 2
+console.log(18/6);
+
+// STEP 3 (POWER OPEN)
+var i = 3 * 4 - ( 12 -[5 * 3] ) + 49
+console.log(7**2);
+
+// STEP 4 (MULTIPLY)
+var i = 12 - ( 12 -[5 * 3] ) + 49
+console.log(3*4);
+
+// STEP 5 (MULTIPLY)
+var i = 12 - ( 12 - 15 ) + 49
+console.log(5*3);
+
+// STEP 6  (SUBTRACTION)
+var i = 12 - (-3) + 49
+
+// STEP 7 
+var i = 12 + 3 + 49
+console.log(12+3+49)
+
+// STEP 8 (FINAL ADDITION)
+var i = 64
+
+console.log(i);
